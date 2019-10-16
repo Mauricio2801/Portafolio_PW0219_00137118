b19ec0d045ded224bbee3ef2ceeae5dc907f401d
Mauricio2801 <00137118@uca.edu.sv>
2019-10-16 10:26:55 -0600

    Tarea

---
+++ Trea_java/main.js
@@ -0,0 +1,35 @@
+function mostrardia1(){
+    document.getElementById('dia1').style.display="block";
+    document.getElementById('dia2').style.display="none";
+    document.getElementById('dia3').style.display="none";
+    document.getElementById('dia4').style.display="none";
+    document.getElementById('dia5').style.display="none";
+}
+
+function mostrardia2(){
+    document.getElementById('dia1').style.display="none";
+    document.getElementById('dia2').style.display="block";
+    document.getElementById('dia3').style.display="none";
+    document.getElementById('dia4').style.display="none";
+    document.getElementById('dia5').style.display="none";
+}
+function mostrardia3(){
+    document.getElementById('dia1').style.display="none";
+    document.getElementById('dia2').style.display="none";
+    document.getElementById('dia3').style.display="block";
+    document.getElementById('dia4').style.display="none";
+    document.getElementById('dia5').style.display="none";
+}
+function mostrardia4(){
+    document.getElementById('dia1').style.display="none";
+    document.getElementById('dia2').style.display="none";
+    document.getElementById('dia3').style.display="none";
+    document.getElementById('dia4').style.display="block";
+    document.getElementById('dia5').style.display="none";
+}
+function mostrardia5(){
+    document.getElementById('dia1').style.display="none";
+    document.getElementById('dia2').style.display="none";
+    document.getElementById('dia3').style.display="none";
+    document.getElementById('dia4').style.display="none";
+    document.getElementById('dia5').style.display="block";
